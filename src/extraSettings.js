b1ff1f9f0077495638dac925128e647dd97d03ca
moziimozzi <78437347+moziimozzi@users.noreply.github.com>
2021-02-08 00:48:33 +0700

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -9,14 +9,14 @@ let extraScore = 1;
 let wuDi = true;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 0;
+let firstFruit = 10;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = true;
 
 // 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
-const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
-const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
+const minRandomFruitNum = reverseLevelUp ? 6 : 6; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
+const maxRandomFruitNum = reverseLevelUp ? 11 : 11; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
 let setFruits = {
   // 指定前几次生成的水果，可填入任意数量的数字，0 为葡萄，9 为半个西瓜
   startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 0, 1, 2, 2, 3],
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://twitter.com/onfmozzi';
+let adLink = false;
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '냥말즈 거꾸로 수박게임';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
