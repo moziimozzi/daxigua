34c7214b8b859af38c00526f490700121cc12300
moziimozzi <78437347+moziimozzi@users.noreply.github.com>
2021-02-07 23:13:02 +0700

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -12,7 +12,7 @@ let wuDi = true;
 let firstFruit = 0;
 
 // 水果合成反转：false 改为 true
-let reverseLevelUp = false;
+let reverseLevelUp = true;
 
 // 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
 const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'https://twitter.com/onfmozzi';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
