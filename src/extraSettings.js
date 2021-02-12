a1033b1d298109ab50c4644b7eb3eba004093fc3
moziimozzi <78437347+moziimozzi@users.noreply.github.com>
2021-02-12 19:18:15 +0700

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -12,7 +12,7 @@ let wuDi = false;
 let firstFruit = 0;
 
 // 水果合成反转：false 改为 true
-let reverseLevelUp = true;
+let reverseLevelUp = false;
 
 // 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
 const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
@@ -26,7 +26,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
